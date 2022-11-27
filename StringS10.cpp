89b5deb26abae2bd6677dd9e5e5a25a735710833
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-11-27 22:40:45 +0530

    [ADDED]Bhavesh:The Program of the Function of String Header File Gets and Puts to take input from User and Display the String respectively

---
+++ StringS10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace::std;
+#include<string.h>
+void displayString(char str[]);
+int main()
+{
+    char str[50];
+    cout<<"Enter string: ";
+    fgets(str, sizeof(str), stdin);             
+    displayString(str);     
+    return 0;
+}
+void displayString(char str[])
+{
+    cout<<"String Output: ";
+    puts(str);
+}
