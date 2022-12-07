4b9b476fbb8a925253d585569452a7ebaf73f63d
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-12-07 18:37:37 +0530

    [ADDED]Bhavesh:The Program of Print the A to Z Alphabets by Using the Do While Loop

---
+++ DoWhileL9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace::std;
+int main()
+{
+    int character=0;
+    char ch='A';
+    do
+    {
+         character=int(ch);
+ 
+         cout<<ch<<" ";
+         character++;
+         ch=char(character);
+    }
+    while(ch<='Z');
+}
