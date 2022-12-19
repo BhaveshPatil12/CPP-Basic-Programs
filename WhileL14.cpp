fb7c18a48a3f7e35409f89beaf0d523e58782ab4
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-12-19 19:56:34 +0530

    [ADDED]Bhavesh:The Program to Print the Value from a Specific Point b by using the While Loop.

---
+++ WhileL14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace::std;
+int main () 
+{
+  int a;
+  cout<<"Enter any Digit"; 
+  cin>>a;
+  while (a>=1 &&a <=10) 
+  {
+    cout<<"\n the value of a is "<< a; 
+    a++;
+  } 
+   return 0;
+}
