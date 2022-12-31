acb51d9d4bc51b9fcbe13265ab81cdad2d8e2f99
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-12-31 19:24:23 +0530

    [ADDED]Bhavesh:The Program to implement wide character and get the size of it using wcslen().

---
+++ StringS22.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+wchar_t c[] = L"Hope never dies" ;
+cout <<"The wide character length of Hope never dies " <<" is : " << wcslen(c) << endl ;
+return 0 ;
+}
