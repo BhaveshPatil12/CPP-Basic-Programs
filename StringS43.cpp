e47e783e204dfe5b55a4564fcee5a92c35c6dec8
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-01-27 16:00:02 +0530

    [ADDED]Bhavesh:The Program to Print All Permutations using BackTracking.

---
+++ StringS43.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+using namespace std;
+void swap (char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+ 
+void permute(char *a, int i, int n)
+{
+    int j;
+    if (i == n)
+        cout<<a<<endl;
+    else
+    {
+        for (j = i; j <= n; j++)
+        {
+            swap((a + i), (a + j));
+            permute(a, i + 1, n);
+            swap((a+i), (a + j));
+       }
+   }
+}
+ 
+int main()
+{
+    char a[] = "abcd";
+    permute(a, 0, 3);
+    return 0;
+}
